f89e95b416709fb78cd7edf10039c1cc639b2dba
Ross McDonald <Ross.McDonald@passivsystems.com>
2013-11-15 12:41:06 +0000

    set femacs (fast emacs) as default editor

--- system/env.zsh
+++ system/env.zsh
@@ -1,5 +1,5 @@
 # Only set this if we haven't set $EDITOR up somewhere else previously.
 if [[ "$EDITOR" == "" ]] ; then
-  # Use emacs for my editor.
-  export EDITOR='emacs'
+  # Use femacs (fast emacs) for my editor - requires that 'es' has launched emacs daemon.
+  export EDITOR='femacs'
 fi
