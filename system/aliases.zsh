ef013ca39d42a98bbd4e2a31426c3a7f0bd3914c
rossputin <rossajmcd@gmail.com>
2013-11-16 22:34:54 +0000

    switch to a more traceable cli colouriser, grc references only

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,13 +3,11 @@
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
+  alias ls="grc ls"
+  alias ll="grc ls -l"
+  alias la='grc ls -la'
 else
   alias ls="ls -F --color"
-  alias l="ls -lAh --color"
   alias ll="ls -l --color"
   alias la="ls -la --color"
 fi
