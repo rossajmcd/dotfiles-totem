974dc23ee56a0512d2561df4068f3273baac4f05
Ross McDonald <Ross.McDonald@passivsystems.com>
2013-11-20 15:26:28 +0000

    add server name to prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -68,11 +68,15 @@ rb_prompt() {
   fi
 }
 
+server_name() {
+  echo "%{$fg_bold[magenta]%}@%m%{$reset_color%}"
+}
+
 directory_name() {
   echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
 }
 
-export PROMPT=$'\n$(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
+export PROMPT=$'\n$(server_name) $(rb_prompt)in $(directory_name) $(git_dirty)$(need_push)\n› '
 set_prompt () {
   export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
 }
